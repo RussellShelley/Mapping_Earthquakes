94c11da4cc91dcf0015520467ee1c937748b3d71
RussellShelley <rjshelley76@gmail.com>
2021-02-26 23:24:15 -0800

    updating eathqquake map

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,79 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY });
+
+// We create the dark view tile layer that will be an option for our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY });
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Street": streets,
+  "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [43.7, -79.3],
+  zoom:11,
+  layers: [satelliteStreets]
+})
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/RussellShelley/Mapping_Earthquakes/main/torontoNeighborhoods.json"
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data,{
+    fillColor: "yellow",
+  color: "blue",
+  weight: 1
+
+}
+).addTo(map);
+
+
+}); 
+
+
+
+
+//Grabbing our GeoJSON data.
+//L.geoJson(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  //pointToLayer: function(feature, latlng) {
+    //console.log(feature);
+    //return L.marker(latlng)
+   //.bindPopup("<h2>" + feature.properties.city + "</h2>");
+ // }
+//}).addTo(map);
+ 
+// L.geoJson(air, {    
+ //   onEachFeature: function(feature, layer) {
+   //   layer.bindPopup("<h2>" + feature.properties.name + "</h2>");
+     //}}) .addTo(map);
+  
+
+
+
+
+
+
+
+// Then we add our 'graymap' tile layer to the map.
+//streets.addTo(map);
+
+
+
